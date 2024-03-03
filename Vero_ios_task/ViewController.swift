328a07e363db36b71cc9631863045049d306754a
Ertuğrul Çinar <ertugrulcnr22@gmail.com>
2024-03-04 00:00:52 +0300

    Initial Commit

---
+++ Vero_ios_task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vero_ios_task
+//
+//  Created by ertugrul on 4.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
